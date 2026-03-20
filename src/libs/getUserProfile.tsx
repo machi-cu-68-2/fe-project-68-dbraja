56b67f0be824efc81728512c0b61ce70402fcd25
Phatthapol <phatthapol.sak@gmail.com>
2026-03-20 15:47:54 +0700

    Add Login

---
+++ src/libs/getUserProfile.tsx
@@ -0,0 +1,16 @@
+export default async function getUserProfile(token: string) {
+
+    const response = await fetch(`http://localhost:5000/api/v1/auth/me`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    });
+
+    if(!response.ok) {
+        throw new Error("Failed to fetch user profile");
+    }
+
+    return await response.json();
+}
