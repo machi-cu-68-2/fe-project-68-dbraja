2953d81ea4ef29aabbfd7777a90ebf9bd26c2f71
Phatthapol <phatthapol.sak@gmail.com>
2026-03-21 13:58:45 +0700

    Change URL_Backend to use vercel

--- src/libs/userLogin.tsx
+++ src/libs/userLogin.tsx
@@ -1,6 +1,6 @@
 export default async function userLogin(userEmail: string, userPassword: string ) {
-
-    const response = await fetch(`http://localhost:5000/api/v1/auth/login`, {
+    console.log("URL:", process.env.BACKEND_URL);
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -1,6 +1,6 @@
 export default async function userRegister(userName: string, userTel: string, userEmail: string, userPassword: string) {
 
-    const response = await fetch(`http://localhost:5000/api/v1/auth/register`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
